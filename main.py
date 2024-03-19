3f65f3be96ed05b329b9069d75e3abfbcca1d860
julio cesar conrado sisnero <conradojuliosisnero@gmail.com>
2024-03-19 11:58:37 -0500

    usando pydantic

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI,Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI();
 app.title = 'Web Movies'
 app.version = '0.0.1'
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview:str
+    year:int
+    rating: float
+    category: str
 
 # peliculas 
 movies = [
@@ -63,27 +73,20 @@ def get_movie_by_category(category: str, year: int):
 
 # metodo post 
 @app.post('/movies',tags=['movies']) 
-def create_movie(id: int = Body(),title:str = Body(),overview: str = Body(),year:int = Body(),rating: float = Body(),category:str = Body()):
-	movies.append({
-		"id":id,
-		"title":title,
-  		"overview":overview,
-		"year":year,
-		"rating": rating,
-		"category": category
-	})
+def create_movie(movie: Movie):
+	movies.append(movie)
 	return movies
 
 # metodo put 
 @app.put('/movies{id}',tags=['movies'])
-def update_movie(id: int,title:str = Body(),overview: str = Body(),year:int = Body(),rating: float = Body(),category:str = Body()):
+def update_movie(id: int,movie: Movie):
 	for item in movies:
 		if item["id"] == id:
-			item['title'] = title
-			item['overview'] = overview
-			item['year'] = year
-			item['rating'] = rating
-			item['category'] = category
+			item['title'] = movie.title
+			item['overview'] = movie.overview
+			item['year'] = movie.year
+			item['rating'] = movie.rating
+			item['category'] = movie.category
 			return movies
 
 # metodo delete 
